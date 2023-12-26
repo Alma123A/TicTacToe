c5a1fcb29dcf9991ee13c98cec4170023f3cf945
Alma123A <alma.tisch1@gmail.com>
2023-12-26 14:36:40 +0200

    define variables

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    String turn;
+    String[][] board;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
