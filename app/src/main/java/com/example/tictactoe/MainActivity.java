0ca0301dfbc3425fdb5223e3a5dc3da1d42bd1cc
Alma123A <alma.tisch1@gmail.com>
2023-12-27 16:33:46 +0200

    action is winner

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,106 +7,174 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-    String turn;
-    String[][] board;
-    int count;
+        String turn;
+        String[][] board;
+        int count;
 
-    @Override
+        private int buttonClickSoundId;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        private static final String TAG = "MainActivity";
 
-    private boolean isWinner() {
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-
-    private void endGame (String st) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("We have a winner!");
-        String msg = "The winner is: " + st;
-        if (st.equals("Tie")) {
-            builder.setTitle("We got a tie");
-            msg = "The result is : " + st;
+            onNewGame();
         }
 
-        builder.setMessage(msg);
-        builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-// Exit handling
-                onDestroy();
+        @Override
+        protected void onStart() {
+            // Call the superclass method first.
+            super.onStart();
 
+        }
+
+        @Override
+        protected void onResume() {
+            // users returns to the activity
+            super.onResume();
+
+        }
+
+        @Override
+        protected void onPause() {
+            // pause the activity and it is no longer available.
+            super.onPause();
+
+        }
+
+        @Override
+        protected void onRestart() {
+            // restarts the activity.
+            super.onRestart();
+
+        }
+
+        @Override
+        protected void onStop() {
+            // the activity is no longer visible.
+            super.onStop();
+
+        }
+
+        @Override
+        protected void onDestroy() {
+            // the activity is finishing or being destroyed by the system.
+            super.onDestroy();
+
+        }
+
+        private boolean isWinner() {
+            for (int i = 0; i < 3; i++) {
+                if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][0].equals("")) {
+                    return true;
+                }
+                if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[0][i].equals("")) {
+                    return true;
+                }
             }
-        });
-        builder.setNegativeButton("Restart", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+
+            // Check diagonals
+            if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals("")) {
+                return true;
+            }
+            if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals("")) {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void endGame(String st) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("We have a winner!");
+            String msg = "The winner is: " + st;
+            if (st.equals("Tie")) {
+                builder.setTitle("We got a tie");
+                msg = "The result is : " + st;
+            }
+
+            builder.setMessage(msg);
+            builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+// Exit handling
+                    onDestroy();
+
+                }
+            });
+            builder.setNegativeButton("Restart", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
 // Cancel handling
 
-            }
-        });
-        AlertDialog dialog = builder.show();
-    }
+                }
+            });
+            AlertDialog dialog = builder.show();
 
-    private void onTurnEnd() {
-        // בדיקה האם יש מנצח - חשוב לבצע לפני הבדיקה אם הלוח מלא
-        if (isWinner())
-            endGame(turn + " won!");
-        else {
-            count++;
-            // בדיקת מצב לוח מלא (תיקו)
-            if (count == 9)
-                endGame("Tie");
+        }
+
+        private void onTurnEnd() {
+            // בדיקה האם יש מנצח - חשוב לבצע לפני הבדיקה אם הלוח מלא
+            if (isWinner())
+                endGame(turn + " won!");
             else {
-                // העברת התור
-                turn = (turn.equals("X") ? "O" : "X");
+                count++;
+                // בדיקת מצב לוח מלא (תיקו)
+                if (count == 9)
+                    endGame("Tie");
+                else {
+                    // העברת התור
+                    turn = (turn.equals("X") ? "O" : "X");
+                }
             }
         }
-    }
-            private void handleClick(int row, int col, int id) {
-                if (board[row][col].equals("")) {
-                    board[row][col] = turn;
-                    Button btn = findViewById(id);
-                    btn.setText(turn);
-                    onTurnEnd();
-        }
-    }
 
-    public void onButtonClick(View view) {
-        int id = view.getId();
-
-        if (id == R.id.btn_00) {
-            handleClick(0, 0, R.id.btn_00);
-        } else if (id == R.id.btn_01) {
-            handleClick(0, 1, R.id.btn_01);
-        } else if (id == R.id.btn_02) {
-            handleClick(0, 2, R.id.btn_02);
-        } else if (id == R.id.btn_10) {
-            handleClick(1, 0, R.id.btn_10);
-        } else if (id == R.id.btn_11) {
-            handleClick(1, 1, R.id.btn_11);
-        } else if (id == R.id.btn_12) {
-            handleClick(1, 2, R.id.btn_12);
-        } else if (id == R.id.btn_20) {
-            handleClick(2, 0, R.id.btn_20);
-        } else if (id == R.id.btn_21) {
-            handleClick(2, 1, R.id.btn_21);
-        } else if (id == R.id.btn_22) {
-            handleClick(2, 2, R.id.btn_22);
+        private void handleClick(int row, int col, int id) {
+            if (board[row][col].equals("")) {
+                board[row][col] = turn;
+                Button btn = findViewById(id);
+                btn.setText(turn);
+                onTurnEnd();
+            }
         }
 
+
+        public void onButtonClick(View view) {
+            int id = view.getId();
+
+            if (id == R.id.btn_00) {
+                handleClick(0, 0, R.id.btn_00);
+            } else if (id == R.id.btn_01) {
+                handleClick(0, 1, R.id.btn_01);
+            } else if (id == R.id.btn_02) {
+                handleClick(0, 2, R.id.btn_02);
+            } else if (id == R.id.btn_10) {
+                handleClick(1, 0, R.id.btn_10);
+            } else if (id == R.id.btn_11) {
+                handleClick(1, 1, R.id.btn_11);
+            } else if (id == R.id.btn_12) {
+                handleClick(1, 2, R.id.btn_12);
+            } else if (id == R.id.btn_20) {
+                handleClick(2, 0, R.id.btn_20);
+            } else if (id == R.id.btn_21) {
+                handleClick(2, 1, R.id.btn_21);
+            } else if (id == R.id.btn_22) {
+                handleClick(2, 2, R.id.btn_22);
+            }
+
+        }
+
+        private void onNewGame() {
+            board = new String[3][3];
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    board[row][col] = new String();
+
+            turn = "X";
+            count = 0;
+        }
+
     }
 
-    private void onNewGame() {
-        board = new String[3][3];
-        for (int row=0; row < 3; row++)
-            for (int col=0; col < 3; col++)
-                board[row][col] = new String();
-
-        turn = "X";
-        count = 0;
-    }
-
-}
